a7692b376a7b3068f331acdb164e482310156872
MiketoCoding <le.michael.v@gmail.com>
2021-09-17 15:57:47 -0400

    Add unshift method to doublylinkedlists
    Unshift method added to doublylinkedlists

--- doublylinkedlists.js
+++ doublylinkedlists.js
@@ -33,6 +33,59 @@ class DoublyLinkedList {
         this.length++
         return this
     }
+
+    // Pop - remove last node on doubly linked list
+    pop() {
+        // if null return undefined
+        if (this.length === 0) return undefined
+        // create a temp variable to equal to this.tail so we can pop it
+        let temp = this.tail
+        // if statement for a signle node in list
+        if (this.length === 1) {
+            this.head = null
+            this.tail = null
+        } else {
+            this.tail = this.tail.prev
+            this.tail.next = null
+            temp.prev = null
+        }
+        this.length--
+        return this
+
+        // instead of below, we can make it easier to read with the code of above
+        // // if null return undefined
+        // if (this.length === 0) return undefined
+        // // create a temp variable to represent this.tail
+        // let temp = this.tail
+        // this.tail = this.tail.prev
+        // this.tail.next = null
+        // temp.prev = null
+        // this.length--
+        // // if length is 0 set head and tail to null
+        // if (this.length === 0) {
+        //     this.head = null
+        //     this.tail = null
+        // }
+        // return temp
+    }
+
+    // unshift - add to the start of the list
+    unshift(value) {
+        // create new node
+        const newNode = new Node(value)
+        // if empty then set head tail to new node
+        if (!this.head) {
+            this.head = newNode
+            this.tail = newNode
+        } else {
+            newNode.next = this.head
+            this.head.prev = newNode
+            this.head = newNode
+        }
+        // increment the length
+        this.length++
+        return this
+    }
 }
 
 let myDoublyLinkedList = new DoublyLinkedList(7)
