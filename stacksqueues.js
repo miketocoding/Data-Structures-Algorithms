92fdc2b3aafddfb27aef5dc5b345de948a0191d8
MiketoCoding <le.michael.v@gmail.com>
2021-09-20 17:49:51 -0400

    Add push method for stacks
    Push method added for stacks

--- stacksqueues.js
+++ stacksqueues.js
@@ -4,6 +4,7 @@ class Node {
         this.next = null
     }
 }
+
 // similar to linked list except we only need top
 class Stack {
     constructor(value) {
@@ -11,4 +12,20 @@ class Stack {
         this.top = newNode
         this.length = 1
     }
+    // renaming unshift to push because this is a stack
+    push(value) {
+        const newNode = new Node(value)
+        if (this.length === 0) {
+            // this.top as opposed to this.head
+            this.top = newNode
+        } else {
+            newNode.next = this.top
+            this.top = newNode
+        }
+        this.length++
+        return this
+    }
 }
+
+
+let myStack = new Stack(11)
