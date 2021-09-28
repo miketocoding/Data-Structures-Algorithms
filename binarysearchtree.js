68b1404abe35e4d1ca1f537cb90e96e27af5e0ec
MiketoCoding <le.michael.v@gmail.com>
2021-09-28 18:27:50 -0400

    Add binary search tree constructor
    Binary Search Tree Constructor added

---
+++ binarysearchtree.js
@@ -0,0 +1,17 @@
+class Node {
+    constructor(value) {
+        this.value = value,
+        this.left = null,
+        this.right = null
+    }
+}
+
+class BST {
+    constructor() {
+        // create framework for binary search tree and don't need to add new node at time tree created
+        this.root = null
+    }
+}
+
+let myTree = new BST()
+myTree
